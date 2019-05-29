827987ecf082294c39e7263fc1dfa394322b17b4
SwitchFun <48801768+SwitchFun@users.noreply.github.com>
2019-05-29 09:48:47 -0500

    Added menu
    credit: @lordapple

--- source/main.c
+++ source/main.c
@@ -1,24 +1,53 @@
 #include <string.h>
 #include <stdio.h>
-
 #include <switch.h>
+void updateConsole(int current){
+	consoleClear();
+	printf("NXToolkit++\n");
+   	printf("\n\x1b[2;4HAutoRCM");
+   	printf("\n\x1b[3;4HExit");
+	printf ("\x1b[%d;2H>", current);
+}
 
 int main(int argc, char **argv)
 {
-    consoleInit(NULL);
+   
+   consoleInit(NULL);
+   
+   int current = 2;
+   int options = 3;
 
+	updateConsole(current);
 
     while(appletMainLoop())
     {
         hidScanInput();
-
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 
-        if (kDown & KEY_PLUS) break;
+		if (kDown & KEY_UP) {
+			current += 1;
+			if (current > options) {
+				current = 2;
+			}
 
+			updateConsole(current);
+		}
+		else if (kDown & KEY_DOWN) {
+			current -= 1;
+			if (current < options && current == 1) {
+				current = options;
+			}
+
+			updateConsole(current);
+		}
+        if (kDown & KEY_PLUS) break;
+		if(kDown & KEY_A && current == 3){
+			break;
+		}
         consoleUpdate(NULL);
     }
-
+	//MARK: EXIT CONSOLE
     consoleExit(NULL);
+	//MARK: RETURN AKA FINISH EXIT
     return 0;
 }
